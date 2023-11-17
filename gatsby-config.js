ac4e243e43b73b20a851bbd5ddc675ef4aa31267
Gabriel <gabriel.antunes@al.infnet.edu.br>
2023-11-16 23:25:47 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Filmódromo`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
